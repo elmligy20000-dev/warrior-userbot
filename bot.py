70aafb85fa2f7352c9179d5cc9a85617f8b64f12
elmligy20000-dev <elmligy20000@gmail.com>
2026-06-26 23:25:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -456,9 +456,8 @@ async def start(event):
 
 <b>اختار وابدأ النشر الاحترافي</b>
 """
-
-        await event.reply(welcome_text, buttons=btns, parse_mode='html')
-        return
+            await event.reply(welcome_text, buttons=btns, parse_mode='html')
+            return
 
     days = get_remaining_days(uid)
     acc = get_account(uid)
